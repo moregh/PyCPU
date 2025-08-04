86526f58b76a9493c7fc21dbefc4e81efc9dac42
matt <matt@styrofoamxylophone.com>
2025-08-04 21:55:52 +0100

    Add new GPU demo

---
+++ scripts/gpu_demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append('../src')
+from python_cpu_emulator.cpu import CPU
+from python_cpu_emulator.compiler import compile
+from python_cpu_emulator.display import Display
+
+
+if __name__ == "__main__":
+    cpu = CPU(gpu=Display())       # Makes a new CPU, with the default RAM value of 64KiB
+    cpu.load_data(compile("../examples/intermediate/pattern.cpu"))
+    while not cpu.halted:
+        cpu.tick()
+    print(cpu) 
+    # This will finish after 38,052 ticks
