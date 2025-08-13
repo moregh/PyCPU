6a62bba8dce305c47b46a43749f432ab29dfa53f
matt <matt@styrofoamxylophone.com>
2025-08-14 00:38:22 +0100

    Update example program location

--- scripts/demo.py
+++ scripts/demo.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     # now open an example program which increments the registers sequentially
     cpu.reset()
     print(cpu)
-    cpu.load_data(compile("../examples/intermediate/counters.cpu"))
+    cpu.load_data(compile("../examples/basic/counters.cpu"))
     while not cpu.halted:
         cpu.tick()
         if cpu.TICKS % 1000000 == 0:
