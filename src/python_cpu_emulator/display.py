3dc7abd6734308638c9c3e1a3c74f9499c14d446
matt <matt@styrofoamxylophone.com>
2025-08-25 01:19:57 +0100

    Display: improve performace
    We now have a target FPS and only draw the screen after the specified interval
    to prevent us redrawing on every single tick of the CPU

--- src/python_cpu_emulator/display.py
+++ src/python_cpu_emulator/display.py
@@ -1,10 +1,19 @@
+from time import time
+
+
 class Display:
-    def __init__(self, width: int = 80, height: int = 50) -> None:
+    def __init__(self, width: int = 80, height: int = 50, fps: int = 120) -> None:
         self.WIDTH: int = width
         self.HEIGHT: int = height
+        self.FPS: int = fps
+        self.interval: float = 1.0 / fps
+        self.last_drawn: float = 0.0
 
     def draw(self, data: list[int]):
         assert len(data) == len(self), f"GPU Error: received {len(data):,} pixels but require {len(self)}"
+        if time() - self.last_drawn < self.interval:
+            return
+        self.last_drawn = time()
         output = ""
         for h in range(self.HEIGHT):
             for w in range(self.WIDTH):
